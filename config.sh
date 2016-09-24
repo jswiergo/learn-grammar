8855e0c7406c121d429dc1392bfae4bb330cb663
Jacek Świergocki <jswiergo@gmail.com>
2016-09-24 12:48:51 +0200

    Add missed config

--- config.sh
+++ config.sh
@@ -4,10 +4,12 @@
 split_dir=split-articles
 finished_dir=finished-articles
 counted_dir=counted-articles
+logs_dir=logs
 
 # Relex
 relex_host=localhost
 relex_port=4445
 
 relex_dir=/home/jacek/github/relex
+relex_server=relex-server-any.sh
 splitter=${relex_dir}/src/split-sentences/split-sentences.pl
