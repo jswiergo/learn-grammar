b334b917f6c1069c241240cea400c66a6c6f7457
Jacek Świergocki <jswiergo@gmail.com>
2017-01-23 18:53:56 +0100

    Add loading word pairs mutual information

--- wordPairs.cc
+++ wordPairs.cc
@@ -1,4 +1,5 @@
 #include <sstream>
+#include <cstdlib>
 #include "wordPairs.h"
 
 using namespace std;
@@ -19,6 +20,50 @@ void WordPairs::insert(const string& word1, const string& word2, int count)
     IdxT index2 = words.insert(word2, 0, count);
     PairIdxT pair_index = make_pair(index1, index2);
     count_word_pair(pair_index, count);
+}
+
+void WordPairs::insert_word_pair_MI(PairIdxT pair_index, entropy_t mi)
+{
+    word_pair_MIs.insert(make_pair(pair_index, mi));
+}
+
+entropy_t WordPairs::get_word_pair_MI(PairIdxT pair_index)
+{
+    if (word_pair_MIs.find(pair_index) != word_pair_MIs.end())
+    {
+        return word_pair_MIs[pair_index];
+    } else {
+        return 0;
+    }
+}
+
+void WordPairs::load_MI(istream& istream)
+{
+    string line;
+    getline(istream, line);
+    if (line == "") return;  // skip empty files
+
+    // skip words
+    IdxT total_words = stoi(line);
+    for (IdxT i = 0; i < total_words; ++i)
+    {
+        getline(istream, line);
+    }
+
+    IdxT total_word_pairs = stoi(line);
+    for (IdxT i = 0; i < total_word_pairs; ++i)
+    {
+        getline(istream, line);
+        stringstream ss(line);
+        string token1, token2, token3;
+        getline(ss, token1, '\t');
+        getline(ss, token2, '\t');
+        getline(ss, token3, '\t');
+        IdxT idx1 = stoi(token1);
+        IdxT idx2 = stoi(token2);
+        entropy_t mi = stod(token3);
+        insert_word_pair_MI(make_pair(idx1, idx2), mi);
+    }
 }
 
 void WordPairs::save(ostream& os)
--- wordPairs.h
+++ wordPairs.h
@@ -6,6 +6,7 @@
 using namespace std;
 
 typedef pair<IdxT, IdxT> PairIdxT;
+typedef double entropy_t;
 
 namespace std
 {
@@ -26,13 +27,18 @@ class WordPairs
 protected:
     Words& words;
     unordered_map<PairIdxT, int> word_pair_counts;
+    unordered_map<PairIdxT, entropy_t> word_pair_MIs;
 
     void count_word_pair(PairIdxT pair_index, int count=1);
+    void insert_word_pair_MI(PairIdxT pair_index, entropy_t mi);
 
 public:
     WordPairs(Words& ws) : words(ws) {};
 
     void insert(const string& word1, const string& word2, int count=1);
+    entropy_t get_word_pair_MI(PairIdxT pair_index);
+
+    void load_MI(istream& is);
     void save(ostream& os);
 
     // translate line by line without memory usage
