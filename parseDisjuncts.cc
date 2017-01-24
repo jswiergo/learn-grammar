07c8d4b1b0a1c1290d5156858979544b883d476c
Jacek Świergocki <jswiergo@gmail.com>
2017-01-24 14:34:19 +0100

    Add maximum spanning tree parsing

--- parseDisjuncts.cc
+++ parseDisjuncts.cc
@@ -1,13 +1,66 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
 #include "algorithms/kruskal.h"
 #include "wordPairs.h"
+#include "disjunct.h"
 
 using namespace std;
 
+typedef Kruskal<IdxT, entropy_t> KruskalT;
+typedef KruskalT::Node NodeT;
+typedef KruskalT::Edge EdgeT;
+
 Words words;
 WordPairs word_pairs(words);
+
+vector<IdxT> parse_word_indexes_from_sentence(const string& sentence)
+{
+    vector<IdxT> word_indexes;
+    stringstream ss(sentence);
+    string token;
+    while (getline(ss, token, '\t'))
+    {
+        word_indexes.push_back(stoi(token));
+    }
+    return word_indexes;
+}
+
+void parse_disjuncts_from_sentence(const string& sentence)
+{
+    vector<IdxT> word_indexes = parse_word_indexes_from_sentence(sentence);
+    size_t number_of_words = word_indexes.size();
+
+    vector<Disjunct> sentence_disjuncts(number_of_words);
+
+    vector<NodeT> nodes;
+    for (IdxT i = 0; i < number_of_words; ++i)
+    {
+        nodes.push_back(i);
+    }
+    vector<EdgeT> edges;
+    for (IdxT i = 0; i < number_of_words-1; ++i)
+    {
+        for (IdxT j = i+1; j < number_of_words; ++j)
+        {
+            entropy_t mi = word_pairs.get_word_pair_MI({word_indexes[i], word_indexes[j]});
+            edges.push_back({{i, j}, mi});
+        }
+    }
+    vector<EdgeT> tree_edges = KruskalT::maximum_spanning_tree(nodes, edges);
+
+}
+
+void parse_disjuncts_from_article(istream& istream)
+{
+    string line;
+    while (getline(istream, line))
+    {
+        parse_disjuncts_from_sentence(line);
+    }
+}
 
 int main(int argc, char** argv)
 {
@@ -33,7 +86,7 @@ int main(int argc, char** argv)
         article_file.open(article_filename);
         output_file.open(output_filename);
 
-        // TODO:
+        parse_disjuncts_from_article(article_file);
 
         article_file.close();
         output_file.close();
